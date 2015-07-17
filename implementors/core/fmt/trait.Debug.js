bb4a12629004fa359c0dd6b8c545f48157cb4c8c
Jeremy Fitzhardinge <jeremy@goop.org>
2015-07-16 23:17:19 -0700

    Add all the other doc stuff

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['hdrhistogram'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='hdrhistogram/struct.F64CountIterItem.html' title='hdrhistogram::F64CountIterItem'>F64CountIterItem</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='hdrhistogram/struct.F64PercentileIterItem.html' title='hdrhistogram::F64PercentileIterItem'>F64PercentileIterItem</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='hdrhistogram/struct.HistoErr.html' title='hdrhistogram::HistoErr'>HistoErr</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='hdrhistogram/struct.CountIterItem.html' title='hdrhistogram::CountIterItem'>CountIterItem</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='hdrhistogram/struct.PercentileIterItem.html' title='hdrhistogram::PercentileIterItem'>PercentileIterItem</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
